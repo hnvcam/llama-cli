577562840cc882ea4149542ccd24695e6b5eb4b3
Cam Hoang <hnvcam@gmail.com>
2026-08-11 19:11:30 +0700

    65K is too less to do a thing

--- kat-coder.sh
+++ kat-coder.sh
@@ -6,8 +6,10 @@ cd ~/Workplace/llama.cpp-ling-flash/build/bin/
 ./llama-server -m /media/hnvcam/AI/LMStudio_Models/mradermacher/KAT-Coder-V2.5-Dev-i1-GGUF/KAT-Coder-V2.5-Dev.i1-Q4_K_M.gguf \
   -ngl 99 \
   -dev CUDA0,CUDA1 \
-  -c 65535 \
-  -ub 256 \
+  -ts 1,0 \
+  -c 131072 \
+  -ub 512 \
+  -ot 'blk\.(23|24|25|26|27|28|29|30|31|32|33|34|35|36|37)\.(ffn_down_exps\.weight|ffn_gate_exps\.weight|ffn_up_exps\.weight)$=CUDA1,^token_embd\.weight$=CUDA1,blk\.(38|39)\.(ffn_down_exps\.weight|ffn_gate_exps\.weight|ffn_up_exps\.weight)$=CPU' \
   -fa on \
   --temp 0.7 --top-p 0.95 --top-k 20 --no-ui \
   --port 1234
--- longcat-lite.sh
+++ longcat-lite.sh
@@ -7,15 +7,13 @@ cd ~/Workplace/llama.cpp-longcat-flash/build/bin/
   -ngl 99 \
   -dev CUDA0,CUDA1 \
   -ts 1,0 \
-  -c 65535 \
+  -c 131072 \
   -ub 512 \
-  -ot 'blk\.(14|16|18|20|22)\.(ffn_down_exps\.weight|ffn_gate_exps\.weight|ffn_up_exps\.weight)$=CUDA1,blk\.(24|26)\.(ffn_down_exps\.weight|ffn_gate_exps\.weight|ffn_up_exps\.weight)$=CPU,^ngram_embd\.=CPU' \
+  -ot 'blk\.(10|12|14|16|18)\.(ffn_down_exps\.weight|ffn_gate_exps\.weight|ffn_up_exps\.weight)$=CUDA1,blk\.(20|22|24|26)\.(ffn_down_exps\.weight|ffn_gate_exps\.weight|ffn_up_exps\.weight)$=CPU,^ngram_embd\.=CPU' \
   -fa on \
   --temp 0.7 --top-p 0.95 --top-k 40 --repeat-penalty 1.06 \
   --jinja --chat-template-file ~/Workplace/llama-cli/longcat-lite.jinja \
   -rea off \
-  --reasoning-budget 8192 \
-  --reasoning-budget-message $'\n\nI have thought long enough, let me answer now.' \
   --port 1234
 
 cd ~/Workplace/llama-cli
